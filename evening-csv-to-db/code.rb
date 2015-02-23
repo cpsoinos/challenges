f814f5fe8d0cfc5f038747e2d2bd81a6fadbe402
Corey Psoinos <coreypsoinos@gmail.com>
2015-02-23 08:55:44 -0500

    Add evening-csv-to-db challenge
    Writing our recipe ingredients to a PSQL server rather than a CSV.

--- evening-csv-to-db/code.rb
+++ evening-csv-to-db/code.rb
@@ -1,1 +1,34 @@
-#YOUR CODE GOES HERE
+require 'pg'
+require 'pry'
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: "recipe")
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+ingredients_to_add = ["1 1/2 pounds Brussels sprouts",
+                      "3 tablespoons good olive oil",
+                      "3/4 teaspoon kosher salt",
+                      "1/2 teaspoon freshly ground black pepper"]
+
+ingredients_to_add.each do |ingredient|
+  db_connection do |conn|
+    conn.exec_params("INSERT INTO ingredients (name) VALUES ($1)", [ingredient])
+  end
+end
+
+ingredients = db_connection { |conn| conn.exec("SELECT name FROM ingredients") }
+
+list_of_ingredients = []
+
+ingredients.each do |ingredient|
+  list_of_ingredients << ingredient["name"]
+end
+
+list_of_ingredients.each do |ingredient|
+  puts "#{list_of_ingredients.index(ingredient) + 1}: #{ingredient}"
+end
