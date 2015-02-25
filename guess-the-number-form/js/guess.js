f89932b34a56a1e0572f6537923479785538606a
Corey Psoinos <coreypsoinos@gmail.com>
2015-02-25 17:35:17 -0500

    Add UL to html and write validate function in js
    Added the div to prepend the analysis message to the HTML file. Wrote a function
    to analyze the user's input, append it to an array, and append a string
    saying whether it is too high, too low, or correct. Submit then adds the message
    to the unordered list.

--- guess-the-number-form/js/guess.js
+++ guess-the-number-form/js/guess.js
@@ -1,1 +1,32 @@
-// YOUR CODE GOES HERE
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+var theNumber = getRandomInt(1, 101);
+var $guess = 0;
+
+console.log(theNumber);
+
+$("#guess-form").on("submit", function(event) {
+  event.preventDefault();
+  $guess = parseInt($("#guess").val(), 10);
+
+  var analysis = validateGuess($guess);
+  var $results = $('<li>').text(analysis[0] + analysis[1]);
+
+  $(".analysis-message").prepend($results);
+});
+
+function validateGuess($guess) {
+  var analysis = [];
+
+  if ($guess > theNumber) {
+    analysis.push($guess, " is too high.")
+  } else if ($guess < theNumber) {
+    analysis.push($guess, " is too low.")
+  } else {
+    analysis.push($guess, " is correct!")
+  };
+
+  return analysis;
+}
