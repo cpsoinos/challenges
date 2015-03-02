56336c178f424f556e31dc691e2c238b33ef22b9
Corey Psoinos <coreypsoinos@gmail.com>
2015-03-02 16:16:01 -0500

    Make game stop at score=2, add reset button
    Made the game stop when one of the players reached a score of 2 (best out of 3).
    When game is over, gives winner a message, and changes Rock Paper and Scissors
    buttons to Reset, which sets the session variables to nil.

--- mmorps/server.rb
+++ mmorps/server.rb
@@ -13,7 +13,6 @@ get "/" do
   end
 
   session[:visit_count] = visit_count + 1
-
   if visit_count == 1
     human_score = 0
     computer_score = 0
@@ -24,37 +23,68 @@ get "/" do
     computer_score = session[:computer_score]
   end
 
+  if visit_count > 1
+    session[:result_message] = "You chose #{session[:human_choice]}. Computer chose #{session[:computer_choice]}. #{session[:result]}."
+  else
+    session[:result_message] = ""
+  end
+
   erb :rps, locals: { human_score: session[:human_score],
-                      computer_score: session[:computer_score] }
+                      computer_score: session[:computer_score],
+                      human_choice: session[:human_choice],
+                      computer_choice: session[:computer_choice],
+                      visit_count: session[:visit_count],
+                      result: session[:result],
+                      result_message: session[:result_message]}
+
 end
 
-post "/choose" do
-  SHAPES = ["Rock", "Paper", "Scissors"]
+SHAPES = ["Rock", "Paper", "Scissors"]
 
+post "/choose" do
   def game
     computer_choice = SHAPES.sample
+    session[:computer_choice] = computer_choice
     human_choice = params["choice"]
+    session[:human_choice] = human_choice
 
     if (human_choice == "Rock" && computer_choice == "Scissors" ) ||
        (human_choice == "Paper" && computer_choice == "Rock") ||
        (human_choice == "Scissors" && computer_choice == "Paper")
+      session[:result] = "You score"
       session[:human_score] = (session[:human_score].to_i + 1).to_i
+      if session[:human_score] == 2
+        session[:game_result] = "You win!"
+      end
     elsif human_choice != computer_choice
+      session[:result] = "Computer scores"
+      session[:computer_score] = (session[:computer_score].to_i + 1).to_i
+      if session[:computer_score] == 2
+        session[:game_result] = "Computer wins!"
+      end
+    else
+      session[:result] = "This round is a tie"
       session[:human_score] = (session[:human_score]).to_i
       session[:computer_score] = (session[:computer_score]).to_i
-    else
-      session[:computer_score] = (session[:computer_score].to_i + 1).to_i
     end
   end
 
-  # until session[:human_score].to_i >= 2 || session[:computer_score].to_i >= 2
-    game
-    binding.pry
-  # # binding.pry
-  #   redirect "/"
-  # end
+  game
+
+  redirect "/"
 
   erb :rps, locals: { rock: params["Rock"],
                       paper: params["Paper"],
-                      scissors: params["Scissors"]}
+                      scissors: params["Scissors"],
+                      result_message: result_message}
+end
+
+post "/reset" do
+  session[:visit_count] = nil
+  session[:human_score] = nil
+  session[:computer_score] = nil
+  session[:result_message] = nil
+  session[:game_result] = nil
+
+  redirect "/"
 end
