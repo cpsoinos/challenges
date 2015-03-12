89214a214136533110debe20414a903b6a0368bd
Corey Psoinos <coreypsoinos@gmail.com>
2015-03-12 10:35:29 -0400

    Move functionality from models to here

--- blackjack/blackjack.rb
+++ blackjack/blackjack.rb
@@ -4,9 +4,8 @@ require_relative 'models/deck'
 require_relative 'models/hand'
 
 deck = Deck.new
-player = Hand.new("Player", deck)
-dealer = Hand.new("Dealer", deck)
-
+player = Hand.new("Player")
+dealer = Hand.new("Dealer")
 
 2.times do
   player.draw(deck.deal!)
@@ -29,31 +28,41 @@ end
 ## PLAYER'S TURN ##
 ###################
 
-until player.bust?("Dealer")
-  if player.prompt == "stand"
-    break
-  end
+until player.bust?
+  current_prompt = player.prompt
   puts
+  if current_prompt == "s"
+    break
+  elsif current_prompt == "h"
+    new_card = player.draw(deck.deal!)
+    puts "Player was dealt #{new_card.last.value} of #{new_card.last.suit}."
+    puts "Player's hand score: #{player.hand_score}"
+  else
+    puts "Stop being a dick and use a letter."
+  end
 end
 
 ###################
 ## DEALER'S TURN ##
 ###################
 
-if player.bust?("Dealer")
-  abort
+if player.bust?
+  abort "BUST! Dealer wins."
 end
 
-puts dealer
+# binding.pry
+puts "Dealer's second card is #{dealer.hand.last.value} of #{dealer.hand.last.suit}."
 puts "Dealer's hand score: #{dealer.hand_score}"
+puts
 
 while dealer.hand_score < 17
   new_card = dealer.draw(deck.deal!).last
   puts "Dealer was dealt #{new_card.value} of #{new_card.suit}."
   puts "Dealer's hand score: #{dealer.hand_score}"
+  puts
 end
 
-if dealer.hand_score > 21
+if dealer.bust?
   puts "BUST! Player wins."
 elsif dealer.hand_score > player.hand_score
   puts "Dealer wins."
