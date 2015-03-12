c47623c8e0083a639ece7cac6730f321df6a8f48
Corey Psoinos <coreypsoinos@gmail.com>
2015-03-12 10:34:49 -0400

    Move VALUES and SUIT constants inside class

--- blackjack/models/card.rb
+++ blackjack/models/card.rb
@@ -1,8 +1,8 @@
+class Card
+
 SUITS  = ["♠", "♥", "♦", "♣"]
 VALUES = ["2", "3", "4", "5", "6", "7", "8",
           "9", "10", "J", "Q", "K", "A"]
-
-class Card
 
   def initialize(value, suit)
     @value = value
@@ -26,6 +26,5 @@ class Card
     end
     score
   end
-
 
 end
