46ce04ee48f5ae67773735c06f751859fb5dacbd
Corey Psoinos <coreypsoinos@gmail.com>
2015-03-13 11:04:35 -0400

    Add methods to BoundingArea to pass tests

--- bounding-box/lib/bounding_area.rb
+++ bounding-box/lib/bounding_area.rb
@@ -1,2 +1,13 @@
 class BoundingArea
+
+  def initialize(array_of_bounding_boxes)
+    @array_of_bounding_boxes = array_of_bounding_boxes
+  end
+
+  attr_accessor :array_of_bounding_boxes
+
+  def contains_point?(x, y)
+    @array_of_bounding_boxes.any? { |box| box.contains_point?(x, y) }
+  end
+
 end
