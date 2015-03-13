825a8525a50b53aaecfa05c5b53a5a4c02e4ae6f
Corey Psoinos <coreypsoinos@gmail.com>
2015-03-13 10:31:07 -0400

    Pass tests for bounding_box class

--- bounding-box/lib/bounding_box.rb
+++ bounding-box/lib/bounding_box.rb
@@ -1,2 +1,27 @@
 class BoundingBox
+
+  def initialize(left, bottom, width, height)
+    @left   = left
+    @bottom = bottom
+    @width  = width
+    @height = height
+  end
+
+  attr_accessor :left, :bottom, :width, :height
+
+  def right
+    left + width
+  end
+
+  def top
+    bottom + height
+  end
+
+  def contains_point?(x,y)
+    x >= left   &&
+    x <= right  &&
+    y >= bottom &&
+    y <= top
+  end
+
 end
