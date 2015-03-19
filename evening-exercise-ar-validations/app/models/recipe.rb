87905d9437c55e38dc74539a5c6be5af73f3ed58
Corey Psoinos <coreypsoinos@gmail.com>
2015-03-19 09:49:35 -0400

    Update validation for yield

--- evening-exercise-ar-validations/app/models/recipe.rb
+++ evening-exercise-ar-validations/app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :name, format: { with: /(brussels sprouts)/i,
     message: "only allows recipes with 'brussels sprouts' in the title" }
-  validates :yield, numericality: { minimum: 1 }
+  validates :yield, numericality: { greater_than_or_equal_to: 1 }
 
   has_many :recipes_comments
   has_many :comments, through: :recipes_comments
