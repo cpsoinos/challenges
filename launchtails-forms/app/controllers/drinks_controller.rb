948badd1297351b25ed01775d44ad9eb4d49ff35
Corey Psoinos <coreypsoinos@gmail.com>
2015-03-24 18:11:31 -0400

    cleanup

--- launchtails-forms/app/controllers/drinks_controller.rb
+++ launchtails-forms/app/controllers/drinks_controller.rb
@@ -17,8 +17,35 @@ class DrinksController < ApplicationController
     @drinks = Drink.page(params[:page])
   end
 
+  def alcohol_level
+    "Rocket Fuel"
+  end
+
+  def alcohol_level_string=(value)
+    case value
+    when "Rocket Fuel"
+      self.alcohol_level = 4
+    when "Strong"
+      self.alcohol_level = 3
+    when "Medium"
+      self.alcohol_level = 2
+    when "Weak"
+      self.alcohol_level = 1
+    when "Non-Alcoholic"
+      self.alcohol_level = 0
+    else
+      self.alcohol_level = nil
+    end
+  end
+
+  def category
+    @drink = Drink.find_by(drink_params)
+    @drink.category
+  end
+
+
   protected
   def drink_params
-    params.require(:drink).permit(:title, :description)
+    params.require(:drink).permit(:title, :description, :category, :alcohol_level)
   end
 end
